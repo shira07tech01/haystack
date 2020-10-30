66669612a30022adc17ac06281e7f56b8d85f494
白濱淳也 <juuuunyaaaa070120@gmail.com>
2020-10-30 09:21:10 +0900

    fix

--- haystack/mecab_wakati.py
+++ haystack/mecab_wakati.py
@@ -1,8 +1,7 @@
 import MeCab
-import sys
 
 def get_wakati_text(jatext:str):
-    m = MeCab.Tagger('-Owakati')
+    m = MeCab.Tagger("-Owakati")
     jatext_wakati = m.parse(jatext)
     return jatext_wakati
 
